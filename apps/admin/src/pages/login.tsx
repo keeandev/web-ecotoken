709c8d89d0115e4bc5e6ad83ddff48a454c6664f
keeandev <38674879+keeandev@users.noreply.github.com>
2023-03-02 22:04:33 -0600

    deployment improvement

--- apps/admin/src/pages/login.tsx
+++ apps/admin/src/pages/login.tsx
@@ -6,50 +6,50 @@ import { toast } from "react-hot-toast";
 import { trpc } from "@/utils/trpc";
 
 const Login = () => {
-	const { mutateAsync, isLoading } = trpc.adminAuth.login.useMutation({
-		onSuccess() {
-			router.push("/");
-			toast.success("Login success.");
-		},
-		onError(e) {
-			toast.error(e.message);
-		}
-	});
-	const router = useRouter();
+    const { mutateAsync, isLoading } = trpc.adminAuth.login.useMutation({
+        onSuccess() {
+            router.push("/");
+            toast.success("Login success.");
+        },
+        onError(e) {
+            toast.error(e.message);
+        },
+    });
+    const router = useRouter();
 
-	const form = useZodForm({
-		schema: loginAdminUserSchema
-	});
+    const form = useZodForm({
+        schema: loginAdminUserSchema,
+    });
 
-	return (
-		<div className="flex h-full w-full items-center justify-center">
-			<Form
-				form={form}
-				onSubmit={async ({ username, password }) => {
-					await mutateAsync({
-						username,
-						password
-					});
-				}}
-				className="m-4 space-y-4 rounded-md bg-slate-200 p-4"
-			>
-				<FormInput
-					label="Username"
-					size="full"
-					{...form.register("username")}
-				/>
-				<FormInput
-					size="full"
-					label="Password"
-					type="password"
-					{...form.register("password")}
-				/>
-				<Button fullWidth loading={isLoading}>
-					Login
-				</Button>
-			</Form>
-		</div>
-	);
+    return (
+        <div className="flex h-full w-full items-center justify-center">
+            <Form
+                form={form}
+                onSubmit={async ({ username, password }) => {
+                    await mutateAsync({
+                        username,
+                        password,
+                    });
+                }}
+                className="m-4 space-y-4 rounded-md bg-slate-200 p-4"
+            >
+                <FormInput
+                    label="Username"
+                    size="full"
+                    {...form.register("username")}
+                />
+                <FormInput
+                    size="full"
+                    label="Password"
+                    type="password"
+                    {...form.register("password")}
+                />
+                <Button fullWidth loading={isLoading}>
+                    Login
+                </Button>
+            </Form>
+        </div>
+    );
 };
 
 Login.getLayout = (page: React.ReactElement) => <>{page}</>;
--- packages/api/src/router/admin/admin-auth.ts
+++ packages/api/src/router/admin/admin-auth.ts
@@ -4,50 +4,55 @@ import { verify } from "argon2";
 import { loginAdminUserSchema } from "../../schema/admin-user";
 
 export const adminAuthRouter = router({
-	login: publicProcedure
-		.input(loginAdminUserSchema)
-		.mutation(async ({ ctx, input }) => {
-			const user = await ctx.prisma.adminUser.findUnique({
-				where: {
-					username: input.username
-				}
-			});
+    login: publicProcedure
+        .input(loginAdminUserSchema)
+        .mutation(async ({ ctx, input }) => {
+            const user = await ctx.prisma.adminUser.findUnique({
+                where: {
+                    username: input.username,
+                },
+            });
+            if (!user)
+                throw new TRPCError({
+                    code: "NOT_FOUND",
+                    message: "A user could not be found.",
+                });
+            else if (!(await verify(user.password, input.password)))
+                throw new TRPCError({
+                    code: "UNAUTHORIZED",
+                    message: "Username or password is incorrect.",
+                });
 
-			if (!user || !(await verify(user.password, input.password)))
-				throw new TRPCError({
-					code: "UNAUTHORIZED",
-					message: "Username or password is incorrect."
-				});
+            const currentDate = new Date(Date.now());
+            const expireDate = currentDate;
+            expireDate.setDate(expireDate.getDate() + 180);
 
-			const currentDate = new Date(Date.now());
-			const expireDate = currentDate;
-			expireDate.setDate(expireDate.getDate() + 180);
+            await ctx.prisma.adminUser.update({
+                where: {
+                    username: input.username,
+                },
+                data: {
+                    hits: user.hits + 1,
+                    lastLogin: currentDate,
+                    expireAt: expireDate,
+                },
+            });
 
-			await ctx.prisma.adminUser.update({
-				where: {
-					username: input.username
-				},
-				data: {
-					hits: user.hits + 1,
-					lastLogin: currentDate,
-					expireAt: expireDate
-				}
-			});
-
-			const firstSite = await ctx.prisma.site.findFirst();
-			ctx.session!.user = {
-				type: "admin",
-				id: user.adminID,
-				ipAddress:
-					process.env.NODE_ENV === "production"
-						? ctx.req.connection.remoteAddress ?? ""
-						: undefined,
-				selectedSite: firstSite?.siteID
-			};
-			await ctx.session!.save();
-		}),
-	logout: adminAuthedProcedure.query(async ({ ctx }) => {
-		ctx.session.destroy();
-		return 200;
-	})
+            const firstSite = await ctx.prisma.site.findFirst();
+            ctx.session!.user = {
+                type: "admin",
+                id: user.adminID,
+                ipAddress:
+                    process.env.NODE_ENV === "production"
+                        ? ctx.req.connection.remoteAddress ?? ""
+                        : undefined,
+                selectedSite: firstSite?.siteID,
+            };
+            console.log("session", ctx.session);
+            await ctx.session!.save();
+        }),
+    logout: adminAuthedProcedure.query(async ({ ctx }) => {
+        ctx.session.destroy();
+        return 200;
+    }),
 });
