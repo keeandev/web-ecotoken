aefcf76ed24f02d7f348440ddb6be42ddd360f53
keeandev <38674879+keeandev@users.noreply.github.com>
2023-03-06 20:14:25 -0600

    fixed css import

--- apps/user/src/pages/_app.tsx
+++ apps/user/src/pages/_app.tsx
@@ -17,9 +17,10 @@ import {
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import { clusterApiUrl } from "@solana/web3.js";
+import { Cluster, clusterApiUrl } from "@solana/web3.js";
 
 import "@solana/wallet-adapter-react-ui/styles.css";
+import { clientEnv } from "@/env/schema.mjs";
 config.autoAddCss = false;
 
 export type NextPageWithLayout = NextPage & {
@@ -41,7 +42,7 @@ const App = ({ Component, pageProps }: AppPropsWithLayout) => {
     const getLayout = Component.getLayout ?? defaultLayout;
 
     // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
-    const network = WalletAdapterNetwork.Devnet;
+    const network = clientEnv.NEXT_PUBLIC_SOLANA_NETWORK as Cluster;
 
     // You can also provide a custom RPC endpoint.
     const endpoint = useMemo(() => clusterApiUrl(network), [network]);
