e20b623f1e8db81fab6109a48d8e2373c2b3b72d
Hunter Jozwiak <hunter.t.joz@gmail.com>
2023-03-14 21:59:26 -0400

    Add a license. (#22)
    * Add a license.
    
    * skywalking-eyes: add the license header configuration.
    
    Fairly bare bones, should be enough for now.
    
    * apps: fix license headers.
    
    * Packages: add license header.
    
    * Add the missing files.
    
    * License new files.
    
    * apps: set license.
    
    * Packages: add license field.
    
    * Add license to the main package.json file.

--- apps/user/src/components/public/layout/footer.tsx
+++ apps/user/src/components/public/layout/footer.tsx
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2023 EcoToken Systems
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import bkgd_wave from "@ecotoken/ui/assets/brand/bkgd_wave.png";
 import white_bevel from "@ecotoken/ui/assets/brand/bevel_white_lg.png";
 import Button from "@ecotoken/ui/components/Button";
